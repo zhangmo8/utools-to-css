b835a9f54ed9fd21a4af24bc544e8600fd68d77b
zhangmo8 <wegi866@gmail.com>
2024-08-16 19:26:00 +0800

    feat: get code & sass parse to css

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,27 @@ import CodeMirror from "~/components/CodeMirror"
 import ThemeIcon from "~/components/ThemeIcon"
 
 import './App.css'
+import { compileString } from "sass"
 
 function App() {
   const storageManager = createLocalStorageManager("ui-theme")
+
+  let codeRef: { getCode: () => string } | undefined;
+
+  const onTransform = () => {
+    console.log('code()', compileString(codeRef?.getCode() || '').css);
+  }
 
   return (
     <>
       <ColorModeScript storageType={storageManager.type} />
       <ColorModeProvider storageManager={storageManager}>
-        <main>
+        <main class="p-12">
           <ThemeIcon />
-          <Button>Transform</Button>
-          <CodeMirror />
+          <Button onclick={onTransform}>Transform</Button>
+          <h1 class="scroll-m-20 text-4xl font-bold">Sass/Scss to Css tool</h1>
+          <p class="my-4 text-balance text-lg sub_title">Enter your SCSS/SASS code, which will be converted to CSS code</p>
+          <CodeMirror ref={codeRef} />
         </main>
       </ColorModeProvider>
     </>
--- src/components/CodeMirror/index.tsx
+++ src/components/CodeMirror/index.tsx
@@ -1,14 +1,14 @@
 import { basicSetup, EditorView } from "codemirror";
 import { sass } from "@codemirror/lang-sass";
 import { Compartment } from '@codemirror/state'
-import { createEffect, onMount } from "solid-js";
-import { githubDark } from "@fsegurai/codemirror-theme-github-dark"
-import { githubLight } from "@fsegurai/codemirror-theme-github-light";
+import { type Component, createEffect, onMount, Ref } from "solid-js";
+import { vitesseLight } from 'codemirror-theme-vitesse/light'
+import { vitesseDark } from 'codemirror-theme-vitesse/dark'
 
 import { useDark } from "~/hooks/useDark";
 import { localStorageManager } from "@kobalte/core";
 
-const CodeMirror = () => {
+const CodeMirror: Component<{ ref?: Ref<{ getCode: () => string }> }> = (props) => {
   let mirrorRef: HTMLDivElement | ((el: HTMLDivElement) => void) | undefined;
   let editorRef: EditorView | undefined;
 
@@ -16,28 +16,38 @@ const CodeMirror = () => {
 
   const [isDark] = useDark()
 
-  createEffect(() => {
-    const theme = isDark() ? githubDark : githubLight;
-
-    editorRef?.dispatch({
-      effects: themeConfig.reconfigure([theme])
-    })
-  })
-
-
   function initMirror() {
     if (mirrorRef) {
       const theme = localStorageManager.get();
-      
+
       editorRef = new EditorView({
-        doc: '// Input your sass/scss code here',
-        extensions: [basicSetup, sass(), themeConfig.of([theme === 'dark' ? githubDark : githubLight])],
+        doc: `// Input your sass/scss code here
+
+              $refd: red;
+
+              .demo {
+                color: $refd;}`,
+        extensions: [basicSetup, sass(), themeConfig.of([theme === 'dark' ? vitesseDark : vitesseLight])],
         parent: mirrorRef as Element,
       });
     }
   }
 
+  function getCode() {
+    return editorRef?.state.doc.toString() || '';
+  }
+
+  props.ref && props.ref({ getCode })
+
   onMount(initMirror)
+
+  createEffect(() => {
+    const theme = isDark() ? vitesseDark : vitesseLight;
+
+    editorRef?.dispatch({
+      effects: themeConfig.reconfigure([theme])
+    })
+  })
 
   return (
     <div ref={mirrorRef}></div>
